4c514d50c611934474a47917e1af8b87252fd068
riatalwar <ria.talwar2006@gmail.com>
2025-01-25 17:00:23 -0500

    implement intersects

--- hw1-starter/src/main/java/hw1/Box.java
+++ hw1-starter/src/main/java/hw1/Box.java
@@ -35,6 +35,11 @@ public class Box extends Sprite {
    */
   public boolean intersects(Box other) {
     // TODO: Implement this method
+    if (other.getX() - other.getWidth() <= getX()
+            && other.getX() + other.getWidth() >= getX()) {
+      return other.getY() - other.getHeight() <= getY()
+              && other.getY() + other.getHeight() >= getY();
+    }
     return false;
   }
 }

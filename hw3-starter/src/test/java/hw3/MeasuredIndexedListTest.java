2ddf198b8ab46278d4e68b3988a7ac0e2cf74dc0
riatalwar <ria.talwar2006@gmail.com>
2025-02-18 21:55:16 -0500

    put and get MeasuredIndexList tests

--- hw3-starter/src/test/java/hw3/MeasuredIndexedListTest.java
+++ hw3-starter/src/test/java/hw3/MeasuredIndexedListTest.java
@@ -32,4 +32,64 @@ public class MeasuredIndexedListTest {
   }
 
   // TODO Add more tests
+  @Test
+  @DisplayName("Get correctly increases access counter")
+  void getIncreasesAccesses() {
+    measuredIndexedList.get(0);
+    measuredIndexedList.get(4);
+    measuredIndexedList.get(7);
+    measuredIndexedList.get(13);
+    assertEquals(4, measuredIndexedList.accesses());
+  }
+
+  @Test
+  @DisplayName("Invalid gets do not increase access counter")
+  void badGetDoesNotIncreaseAccesses() {
+    try {
+      measuredIndexedList.get(15);
+      fail("Expected error");
+    } catch (IndexException e) {
+      assertEquals(0, measuredIndexedList.accesses());
+    }
+
+    measuredIndexedList.get(4);
+    measuredIndexedList.get(7);
+
+    try {
+      measuredIndexedList.get(32);
+      fail("Expected error");
+    } catch (IndexException e) {
+      assertEquals(2, measuredIndexedList.accesses());
+    }
+  }
+
+  @Test
+  @DisplayName("Put correctly increases mutation counter")
+  void putIncreasesMutations() {
+    measuredIndexedList.put(0, 4);
+    measuredIndexedList.put(4, 7);
+    measuredIndexedList.put(12, 2);
+    assertEquals(3, measuredIndexedList.mutations());
+  }
+
+  @Test
+  @DisplayName("Invalid puts do not increase mutation counter")
+  void badPutDoesNotIncreaseMutations() {
+    try {
+      measuredIndexedList.put(15, 10);
+      fail("Expected error");
+    } catch (IndexException e) {
+      assertEquals(0, measuredIndexedList.mutations());
+    }
+
+    measuredIndexedList.put(4, 7);
+    measuredIndexedList.put(12, 2);
+
+    try {
+      measuredIndexedList.put(53, 1);
+      fail("Expected error");
+    } catch (IndexException e) {
+      assertEquals(2, measuredIndexedList.mutations());
+    }
+  }
 }

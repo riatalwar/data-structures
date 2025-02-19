bcafb486c3e30a8bd3012ea6669bc7bf3ff8fc21
riatalwar <ria.talwar2006@gmail.com>
2025-02-18 22:05:47 -0500

    import

--- hw3-starter/src/test/java/hw3/MeasuredIndexedListTest.java
+++ hw3-starter/src/test/java/hw3/MeasuredIndexedListTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MeasuredIndexedListTest {
 

34ef0ffe99a288c2908f068414f0eeafac79f690
riatalwar <ria.talwar2006@gmail.com>
2025-02-22 19:42:00 -0500

    transpose main function

--- hw3-starter/src/main/java/hw3/search/TransposeArraySet.java
+++ hw3-starter/src/main/java/hw3/search/TransposeArraySet.java
@@ -7,6 +7,25 @@ package hw3.search;
  */
 public class TransposeArraySet<T> extends ArraySet<T> {
 
+  public static void main(String[] args) {
+    TransposeArraySet<Integer> ls = new TransposeArraySet<>();
+
+    ls.insert(2);
+    ls.insert(3);
+    ls.insert(8);
+    ls.insert(4);
+    ls.insert(6);
+
+    ls.remove(3);
+    ls.remove(4);
+
+    ls.insert(5);
+
+    ls.has(3);
+    ls.has(8);
+    ls.has(5);
+  }
+
   // transpose-sequential-search heuristic
   @Override
   protected int find(T t) {

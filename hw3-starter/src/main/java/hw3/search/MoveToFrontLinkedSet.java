866afc791f8ccea43d59539264a68b57bf3d4da2
riatalwar <ria.talwar2006@gmail.com>
2025-02-22 19:41:48 -0500

    movetofront main function

--- hw3-starter/src/main/java/hw3/search/MoveToFrontLinkedSet.java
+++ hw3-starter/src/main/java/hw3/search/MoveToFrontLinkedSet.java
@@ -7,6 +7,25 @@ package hw3.search;
  */
 public class MoveToFrontLinkedSet<T> extends LinkedSet<T> {
 
+  public static void main(String[] args) {
+    MoveToFrontLinkedSet<Integer> ls = new MoveToFrontLinkedSet<>();
+
+    ls.insert(2);
+    ls.insert(3);
+    ls.insert(8);
+    ls.insert(4);
+    ls.insert(6);
+
+    ls.remove(3);
+    ls.remove(4);
+
+    ls.insert(5);
+
+    ls.has(3);
+    ls.has(8);
+    ls.has(5);
+  }
+
   // move-to-front heuristic
   @Override
   protected Node<T> find(T t) {

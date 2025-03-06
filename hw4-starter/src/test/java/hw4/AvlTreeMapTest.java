fec84bbb4eb02b410197974a5010f75517285f93
riatalwar <ria.talwar2006@gmail.com>
2025-03-06 16:19:25 -0500

    reorganize tests

--- hw4-starter/src/test/java/hw4/AvlTreeMapTest.java
+++ hw4-starter/src/test/java/hw4/AvlTreeMapTest.java
@@ -18,38 +18,6 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
     return new AvlTreeMap<>();
   }
 
-  @Test
-  public void insertLeftRotation() {
-    map.insert("1", "a");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        1:a
-     */
-
-    map.insert("2", "b");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        1:a,
-        null 2:b
-     */
-
-    map.insert("3", "c"); // it must do a left rotation here!
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        2:b,
-        1:a 3:c
-     */
-
-    String[] expected = new String[]{
-        "2:b",
-        "1:a 3:c"
-    };
-    assertEquals((String.join("\n", expected) + "\n"), map.toString());
-  }
-
   // TODO Add more tests
 
   @Test
@@ -119,6 +87,39 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
             "2:a",
             "1:b 3:c",
             "4:d"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+
+  @Test
+  @DisplayName("insert() correctly executes a left rotation.")
+  public void insertLeftRotation() {
+    map.insert("1", "a");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        1:a
+     */
+
+    map.insert("2", "b");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        1:a,
+        null 2:b
+     */
+
+    map.insert("3", "c"); // it must do a left rotation here!
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        2:b,
+        1:a 3:c
+     */
+
+    String[] expected = new String[]{
+            "2:b",
+            "1:a 3:c"
     };
     assertEquals((String.join("\n", expected) + "\n"), map.toString());
   }

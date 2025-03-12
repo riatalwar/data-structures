9f014daed57bea318df70fde695ac2e0f1be881b
riatalwar <ria.talwar2006@gmail.com>
2025-03-12 17:41:58 -0400

    treap testing

--- hw4-starter/src/test/java/hw4/TreapMapTest.java
+++ hw4-starter/src/test/java/hw4/TreapMapTest.java
@@ -1,5 +1,12 @@
 package hw4;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * In addition to the tests in BinarySearchTreeMapTest (and in OrderedMapTest & MapTest),
  * we add tests specific to Treap.
@@ -9,10 +16,23 @@ public class TreapMapTest extends BinarySearchTreeMapTest {
 
   @Override
   protected Map<String, String> createMap() {
-    return new TreapMap<>();
+    Random rand = new Random();
+    rand.setSeed(0);
+    /*  0 -1155484576 (4)
+        1  -723955400 (6)
+        2  1033096058 (8)
+        3 -1690734402 (2)
+        4 -1557280266 (3)
+        5  1327362106 (9)
+        6 -1930858313 (0)
+        7   502539523 (7)
+        8 -1728529858 (1)
+        9  -938301587 (5)
+    */
+    return new TreapMap<>(rand);
   }
 
-  // insert right rotation
+
   @Test
   @DisplayName("insert() results in right rotations")
   public void insertRightRotation() {
@@ -28,7 +48,7 @@ public class TreapMapTest extends BinarySearchTreeMapTest {
     };
     assertEquals((String.join("\n", expected) + "\n"), map.toString());
   }
-  // insert left rotation
+
   @Test
   @DisplayName("insert() results in left rotations")
   public void insertLeftRotation() {
@@ -44,7 +64,7 @@ public class TreapMapTest extends BinarySearchTreeMapTest {
     };
     assertEquals((String.join("\n", expected) + "\n"), map.toString());
   }
-  // insert multiple rotations
+
   @Test
   @DisplayName("insert() results in multiple rotations")
   public void insertMultipleRotation() {
@@ -58,6 +78,25 @@ public class TreapMapTest extends BinarySearchTreeMapTest {
             "2:a:-1155484576 4:c:1033096058",
             "1:b:-723955400 null null null"
     };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+
+  @Test
+  @DisplayName("insert() works in complex case with multiple rotations")
+  public void insertComplex() {
+    map.insert("8", "a");
+    map.insert("6", "b");
+    map.insert("1", "c");
+    map.insert("3", "d");
+    map.insert("7", "e");
+    map.insert("2", "f");
+
+    String[] expected = new String[]{
+            "3:d:-1690734402",
+            "1:c:1033096058 7:e:-1557280266",
+            "null 2:f:1327362106 6:b:-723955400 8:a:-1155484576"
+    };
+
     assertEquals((String.join("\n", expected) + "\n"), map.toString());
   }
 
@@ -130,7 +169,25 @@ public class TreapMapTest extends BinarySearchTreeMapTest {
     assertEquals((String.join("\n", expected) + "\n"), map.toString());
   }
 
-  // TODO Add tests
-  //  (think about how you might write tests while randomness is involved in TreapMap implementation!)
+  @Test
+  @DisplayName("remove() correctly executes multiple rotations")
+  public void removeMultipleRotations() {
+    map.insert("8", "a");
+    map.insert("6", "b");
+    map.insert("1", "c");
+    map.insert("3", "d");
+    map.insert("7", "e");
+    map.insert("2", "f");
 
+    map.remove("3");
+
+    String[] expected = new String[]{
+            "7:e:-1557280266",
+            "6:b:-723955400 8:a:-1155484576",
+            "1:c:1033096058 null null null",
+            "null 2:f:1327362106 null null null null null null"
+    };
+
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
 }

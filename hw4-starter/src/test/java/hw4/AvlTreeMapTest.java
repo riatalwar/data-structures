7a5194b602eb91bb63e1b905d7d2d10647596d04
riatalwar <ria.talwar2006@gmail.com>
2025-03-13 12:10:46 -0400

    unused import

--- hw4-starter/src/test/java/hw4/AvlTreeMapTest.java
+++ hw4-starter/src/test/java/hw4/AvlTreeMapTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * In addition to the tests in BinarySearchTreeMapTest (and in OrderedMapTest & MapTest),
